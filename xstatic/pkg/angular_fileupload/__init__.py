04bcd774dae17cfff5e826fc0874f6403b4a6cae
Jordan OMara <jomara@redhat.com>
2014-09-09 09:34:11 -0400

    Incorrect display name

--- xstatic/pkg/angular_fileupload/__init__.py
+++ xstatic/pkg/angular_fileupload/__init__.py
@@ -4,16 +4,16 @@ XStatic resource package
 See package 'XStatic' for documentation and basic tools.
 """
 
-DISPLAY_NAME = 'Angular-Bootstrap' # official name, upper/lowercase allowed, no spaces
+DISPLAY_NAME = 'Angular-FileUpload' # official name, upper/lowercase allowed, no spaces
 PACKAGE_NAME = 'XStatic-%s' % DISPLAY_NAME # name used for PyPi
 
 NAME = __name__.split('.')[-1] # package name (e.g. 'foo' or 'foo_bar')
                                # please use a all-lowercase valid python
                                # package name
 
-VERSION = '0.11.0' # version of the packaged files, please use the upstream
+VERSION = '1.4.0' # version of the packaged files, please use the upstream
                   # version number
-BUILD = '2' # our package build number, so we can release new builds
+BUILD = '1' # our package build number, so we can release new builds
              # with fixes for xstatic stuff.
 PACKAGE_VERSION = VERSION + '.' + BUILD # version used for PyPi
 
