d964a4e6473158c978ef8acaea2c95b7a0627e83
Rob Cresswell <robert.cresswell@outlook.com>
2016-05-13 12:00:02 +0100

    Update XStatic-Angular-FileUpload to 12.0.4.0
    Change-Id: I22d52fd33c4263f7c44db571605280917ef34476

--- xstatic/pkg/angular_fileupload/__init__.py
+++ xstatic/pkg/angular_fileupload/__init__.py
@@ -11,9 +11,9 @@ NAME = __name__.split('.')[-1] # package name (e.g. 'foo' or 'foo_bar')
                                # please use a all-lowercase valid python
                                # package name
 
-VERSION = '1.4.0' # version of the packaged files, please use the upstream
+VERSION = '12.0.4' # version of the packaged files, please use the upstream
                   # version number
-BUILD = '2' # our package build number, so we can release new builds
+BUILD = '0' # our package build number, so we can release new builds
              # with fixes for xstatic stuff.
 PACKAGE_VERSION = VERSION + '.' + BUILD # version used for PyPi
 
@@ -24,8 +24,8 @@ CLASSIFIERS = []
 KEYWORDS = '%s xstatic' % NAME
 
 # XStatic-* package maintainer:
-MAINTAINER = 'Jordan OMara'
-MAINTAINER_EMAIL = 'jsomara@gmail.com'
+MAINTAINER = 'Rob Cresswell'
+MAINTAINER_EMAIL = 'robert.cresswell@outlook.com'
 
 # this refers to the project homepage of the stuff we packaged:
 HOMEPAGE = 'https://github.com/danialfarid/angular-file-upload'
